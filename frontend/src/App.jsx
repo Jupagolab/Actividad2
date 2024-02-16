b6ba5aa4c298a220114677d1797ff45661cbd8f7
JMMP33 <94369965+JMMP33@users.noreply.github.com>
2024-02-16 18:07:02 -0400

    Inclusion del modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 //import { useState } from 'react'
 import './App.css'
+import Modal from './assets/modal';
 import Lista from './assets/Lista';
+
 
 function App() {
   const API = 'http://localhost:4000/inventario/';
@@ -22,8 +24,10 @@ function App() {
       <main className="flex flex-wrap text-center p-4 bg-verde-500 min-h-4/5">
         <h2>Bienvenidos</h2>
         <Lista 
+
           api={API}
         />
+        <Modal/>
       </main>
     </>
   )
