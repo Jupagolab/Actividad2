e26d064d2ddb439375934da3c9ea4b1be9162da2
Jupagolab <jupagolab@gmail.com>
2024-02-16 20:04:43 -0400

    backend cambios

--- backend/src/controllers/usuarios-c.js
+++ backend/src/controllers/usuarios-c.js
@@ -1,27 +1,59 @@
 const usuarios = require('../models/usuarios');
 const { SECRET } = process.env;
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('../middleware/bcrypt');
 
 class UsuarioController {
   crearUsuario = async (req, res) => {
     try {
-      const { usuario, nombre, correo, clave, rol } = req.body;
-      
-      // Encriptar la contraseña
-      const claveEncriptada = await bcrypt.hash(clave, 10);
-      req.body.clave = claveEncriptada;
+      await bcrypt.encriptarContrasenaMiddleware(req, res, async () => {
+        const { usuario, nombre, correo, clave, rol } = req.body;
 
-      const nuevoUsuario = new usuarios({ usuario, nombre, correo, clave, rol });
-      await nuevoUsuario.save();
+        const nuevoUsuario = new usuarios({ usuario, nombre, correo, clave, rol });
+        await nuevoUsuario.save();
 
-      // Crear un token para el nuevo usuario
-      const token = jwt.sign({ 'usuario': usuario, 'rol': rol }, SECRET, { expiresIn: '1h' });
-
-      res.json({ 'token': token, mensaje: 'Usuario registrado con éxito' });
+        res.json({ mensaje: 'Usuario registrado con éxito' });
+      })
     } catch (error) {
       console.error(error);
       res.status(500).json({ mensaje: `Error en el servidor: ${error}` });
+    }
+  }
+
+  iniciarSesion = async (req, res) => {
+    try {
+
+      if (!req.body.usuario || !req.body.clave) {
+        return res.status(500).json({ mensaje: "Se requiere Usuario y Contraseña para inciar sesión" });
+      }
+
+      const { usuario, clave } = req.body;
+
+      const buscarUsuario = await usuarios.findOne({ usuario });
+
+      if (!buscarUsuario) {
+        return res.status(500).json({ mensaje: "Credenciales inválidas" });
+      }
+
+      // Verificar la contraseña
+      const claveValida = await bcrypt.compare(clave, usuarioEncontrado.clave);
+
+      // Si la contraseña es inválida, enviar una respuesta de error
+      if (!claveValida) {
+        return res.status(401).json({ mensaje: 'Contraseña inválida' })
+      }
+
+      const token = tokenSign({
+        usuario: buscarUsuario.usuario,
+        rol: buscarUsuario.rol,
+      });
+
+      res.json({ 'token': token })
+
+
+
+    } catch (err) {
+
     }
   }
 }
--- backend/src/middleware/bcrypt.js
+++ backend/src/middleware/bcrypt.js
@@ -6,4 +6,23 @@ const encriptarContrasena = async (contrasena) => {
   return hash;
 };
 
-module.exports = encriptarContrasena;
+// Middleware para encriptar la contraseña antes de guardarla
+const encriptarContrasenaMiddleware = async (req, res, next) => {
+  try {
+    if (!req.body.clave) {
+      return next();
+    }
+    const hash = await encriptarContrasena(req.body.clave);
+    req.body.clave = hash;
+    next();
+  } catch (error) {
+    res.status(500).json({mensaje: 'Error al encriptar la contraseña', error});
+  }
+};
+
+const compare = async (Clave, ClaveEncrypt) => {
+  return await bcrypt.compare(Clave,ClaveEncrypt)
+}
+
+
+module.exports = { encriptarContrasenaMiddleware, compare };
